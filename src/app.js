038d4779922c9ff8ad6a9e8708767e3673b1d425
martin835 <64438132+martin835@users.noreply.github.com>
2022-05-06 16:42:01 +0200

    cors fixed

--- src/app.js
+++ src/app.js
@@ -12,8 +12,29 @@ import chatRouter from "./services/chats/index.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
+const whitelist = [
+  process.env.FE_DEV_URL,
+  process.env.FE_PROD_URL,
+  process.env.REDIRECT,
+];
 
-app.use(cors({ origin: process.env.REDIRECT, credentials: true }));
+app.use(
+  cors({
+    origin: function (origin, next) {
+      //cors is a global middleware - for each request
+      console.log("ORIGIN: ", origin);
+      // 0 \\ 0
+      if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+        console.log("ORIGIN ALLOWED");
+        next(null, true);
+      } else {
+        console.log("ORIGIN NOT ALLOWED");
+        next(new Error("CORS ERROR!"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
